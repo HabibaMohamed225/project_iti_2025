9dfb8226cd09a6fae7cd36bc675f505fa129684f
SHadya_Fathy <shadyafathy80@gmail.com>
2025-08-19 16:55:04 +0300

    shadya update

--- lib/presentation/screens/cart/cart_screen.dart
+++ lib/presentation/screens/cart/cart_screen.dart
@@ -6,37 +6,31 @@ import 'package:project_iti_2025/core/constants/app_colors.dart';
 import 'package:project_iti_2025/core/constants/app_strings.dart';
 import 'package:project_iti_2025/presentation/widgets/auth/app_bottom_nav.dart';
 import 'cart_screen_content.dart';
-
 class CartScreen extends StatelessWidget {
-  const CartScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: BlocBuilder<CartBloc, CartState>(
-          builder: (context, state) {
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  AppStrings.yourCart,
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+const CartScreen({super.key});
+@override
+Widget build(BuildContext context) {
+final theme = Theme.of(context).textTheme;
+return Scaffold( 
+  appBar: AppBar(
+     title: BlocBuilder<CartBloc, CartState>
+     ( builder: (context, state) 
+     { return Column(
+       crossAxisAlignment:
+        CrossAxisAlignment.start,
+         children: [
+           Text( AppStrings.yourCart,
+            style: theme.titleLarge?.copyWith( color: AppColors.primaryTextColor,
+             fontWeight: FontWeight.bold, ), ),
+              Text( "Items: ${state.itemCount}",
+               style: theme.bodySmall?.copyWith(color: AppColors.darkGrey),
                 ),
-                Text(
-                  '${state.itemCount} items',
-                  style: const TextStyle(
-                    fontSize: 14,
-                    color: AppColors.darkGrey,
-                  ),
-                ),
-              ],
-            );
-          },
-        ),
-      ),
-      body: const CartScreenContent(),
-      bottomNavigationBar: const AppBottomNav(currentIndex: 1),
-    );
-  }
+                 ],
+                  );
+                   },
+                    ),
+                     ),
+                      body: const CartScreenContent(),
+                       bottomNavigationBar: const AppBottomNav(currentIndex: 1), ); 
+}
 }
